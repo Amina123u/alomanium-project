5fc711f09a190a562eb7e935bcf3daf9b4293ab6
Amina123u <umimaher786@gmail.com>
2025-08-02 22:01:28 +0500

    text

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const swiper = new Swiper(".testimonial-swiper", {
+  loop: true,
+  autoplay: { delay: 3000 },
+  pagination: { el: ".swiper-pagination" },
+  direction: "rtl",
+  spaceBetween: 20,
+});
